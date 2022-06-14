b76a3b3367025106aaf6da75f125391bf1937d65
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-06-14 00:36:11 -0500

    returning choice array workedgit add -A ' q

--- Develop/script.js
+++ Develop/script.js
@@ -4,15 +4,18 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var lowerCase = ['a', 'b','c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var specialCharacters= ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
-var ArrayofCharacters = [];
+var choiceArray = [];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
   console.log("Hey!  You clicked the button!")
-
-  return "Generated Password will go here";
+  
+  passwordType();
+  return choiceArray;
+  
+  //return "Generated Password will go here";
 }
 // Write password to the #password input
 function writePassword() {
@@ -48,9 +51,18 @@ function passwordType(){
    if (specialCharactersQuery) {
      choiceArray= choiceArray.concat(specialCharacters);
    }
-   passwordType();
+   console.log(choiceArray);
+   return choiceArray;
 }
-
+// function for password length
 function passwordLength(){
-  
+  characterLength = parseInt(prompt("Between 8 and 128, how many characters would you like in your password?"));
+  if (isNaN(characterLength) || characterLength < 8 || characterLength > 8) {
+    window.alert("You must pick a number between 8 and 128.");
+    passwordLength();
+  }
+
+  else if (characterLength >=8 && characterLength <=128)
+    window.alert(characterLength + "is a good number.")
+  console.log(characterLength);
 }
